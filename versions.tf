54c3b3754c49f06745e54c090c37818e354f3a2d
jimi-117 <sho.jimi117@gmail.com>
2026-09-23 13:32:40 +0200

    #02 add provider file and initialized

--- versions.tf
+++ versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.16.0, < 2.0.0"
 
-  required_providers = {
+  required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "=5.6.0"
